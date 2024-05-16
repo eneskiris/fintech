7ff44567ff9a94887addd3d5823171a65cb87cdf
enes <eneskiris@gmail.com>
2024-05-16 23:23:11 +0300

    adds prebuild

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Page = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello, world!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Page;
