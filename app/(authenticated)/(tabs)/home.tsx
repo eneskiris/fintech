62e21d137068f7a44108df41e2336f19b8626ae5
enes <eneskiris@gmail.com>
2024-05-19 19:33:17 +0300

    adds home screen with native dropdown

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,11 +1,78 @@
-import { Text, View } from "react-native";
+import Dropdown from "@/components/Dropdown";
+import RoundBtn from "@/components/RoundBtn";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useHeaderHeight } from "@react-navigation/elements";
 
 const Page = () => {
+  const balance = 350;
+  const headerHeight = useHeaderHeight();
+
+  const onAddMoney = () => {};
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView
+      style={{ backgroundColor: Colors.background }}
+      contentContainerStyle={{
+        paddingTop: headerHeight,
+      }}
+    >
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.currency}>€</Text>
+        </View>
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButtonSmall,
+            { backgroundColor: Colors.lightGray, marginVertical: 20 },
+          ]}
+        >
+          <Text style={[defaultStyles.buttonTextSmall, { color: Colors.dark }]}>
+            Accounts
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.actionRow}>
+        <RoundBtn icon={"add"} text={"Add money"} onPress={onAddMoney} />
+        <RoundBtn icon={"refresh"} text={"Exchange"} />
+        <RoundBtn icon={"list"} text={"Details"} />
+        <Dropdown />
+      </View>
+    </ScrollView>
   );
 };
-
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    gap: 10,
+  },
+  balance: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+});
 export default Page;
