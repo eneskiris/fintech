aa829413082d5677ceaca6ec229e61214ad5fef1
enes <eneskiris@gmail.com>
2024-05-20 22:16:35 +0300

    adds zustand and mmkv for balance

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -2,6 +2,8 @@ import Dropdown from "@/components/Dropdown";
 import RoundBtn from "@/components/RoundBtn";
 import Colors from "@/constants/Colors";
 import { defaultStyles } from "@/constants/Styles";
+import { useBalanceStore } from "@/store/balanceStore";
+import { Ionicons } from "@expo/vector-icons";
 import {
   ScrollView,
   StyleSheet,
@@ -12,10 +14,18 @@ import {
 import { useHeaderHeight } from "@react-navigation/elements";
 
 const Page = () => {
-  const balance = 350;
+  const { balance, runTransaction, transactions, clearTransactions } =
+    useBalanceStore();
   const headerHeight = useHeaderHeight();
 
-  const onAddMoney = () => {};
+  const onAddMoney = () => {
+    runTransaction({
+      id: Math.random().toString(),
+      amount: Math.floor(Math.random() * 1000) * (Math.random() > 0.5 ? 1 : -1),
+      date: new Date(),
+      title: "Added money",
+    });
+  };
 
   return (
     <ScrollView
@@ -26,7 +36,7 @@ const Page = () => {
     >
       <View style={styles.account}>
         <View style={styles.row}>
-          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.balance}>{balance()}</Text>
           <Text style={styles.currency}>€</Text>
         </View>
         <TouchableOpacity
@@ -43,9 +53,44 @@ const Page = () => {
 
       <View style={styles.actionRow}>
         <RoundBtn icon={"add"} text={"Add money"} onPress={onAddMoney} />
-        <RoundBtn icon={"refresh"} text={"Exchange"} />
+        <RoundBtn
+          icon={"refresh"}
+          text={"Exchange"}
+          onPress={clearTransactions}
+        />
         <RoundBtn icon={"list"} text={"Details"} />
         <Dropdown />
+      </View>
+
+      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+      <View style={styles.transactions}>
+        {transactions.length === 0 && (
+          <Text style={{ padding: 14, color: Colors.gray }}>
+            No transactions yet
+          </Text>
+        )}
+        {transactions.map((transaction) => (
+          <View
+            key={transaction.id}
+            style={{ flexDirection: "row", alignItems: "center", gap: 16 }}
+          >
+            <View style={styles.circle}>
+              <Ionicons
+                name={transaction.amount > 0 ? "add" : "remove"}
+                size={24}
+                color={Colors.dark}
+              />
+            </View>
+
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontWeight: "400" }}>{transaction.title}</Text>
+              <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                {transaction.date.toLocaleString()}
+              </Text>
+            </View>
+            <Text>{transaction.amount}€</Text>
+          </View>
+        ))}
       </View>
     </ScrollView>
   );
@@ -73,6 +118,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     padding: 20,
+  },
+  transactions: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    gap: 20,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Colors.lightGray,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 export default Page;
